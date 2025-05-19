867201ac842a419a8084603aaaaf92a5351492dd
loraxy12 <92580916+loraxy12@users.noreply.github.com>
2025-05-19 19:17:39 +0200

    törlés
    reégi törlése

--- src/app/nagy.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NagyPipe } from './nagy.pipe';
-
-describe('NagyPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NagyPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
